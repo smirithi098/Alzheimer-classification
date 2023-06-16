425b2415483fbe0ce310a46e95054c8d1288e82a
Smirithi <smirithi898@gmail.com>
2023-06-16 10:45:08 +0100

    Add analysis file V2

--- alzheimer_classification.R
+++ alzheimer_classification.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(ggplot2)
 library(superml)
 library(tidyr)
+library(plotly)
+library(ggExtra)
 
 # read the csv file as dataframe
 raw_data <- read.csv("project data.csv", header = TRUE)
@@ -73,6 +75,8 @@ raw_data$eTIV <- (raw_data$eTIV - eTIV_mean_sd$mean_col)/ eTIV_mean_sd$sd_col
 raw_data$nWBV <- (raw_data$nWBV - nWBV_mean_sd$mean_col)/ nWBV_mean_sd$sd_col
 raw_data$ASF <- (raw_data$ASF - asf_mean_sd$mean_col)/ asf_mean_sd$sd_col
 
+raw_data$CDR <- as.factor(raw_data$CDR)
+
 clean_data <- raw_data
 
 #------------------------DESCRIPTIVE ANALYSIS-----------------------------------
@@ -80,26 +84,77 @@ clean_data <- raw_data
 # get the summary of the data fields
 summary(clean_data)
 
-# distribution of age of all patients
-clean_data %>%
-  ggplot(aes(x=Age)) +
-  geom_histogram(aes(y=..density..), color="coral3", fill="coral3") +
-  geom_density(alpha=0.3, fill="bisque") +
+# Number of patients with different CDR for each gender
+
+ggplotly(clean_data %>%
+  select(c("Gender", "CDR")) %>%
+  group_by(Gender, CDR) %>%
+  summarise(count_n = n()) %>%
+  ggplot(aes(x = Gender, y = count_n, fill = CDR)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  scale_fill_manual(values = colorspace::diverge_hcl(4)) +
+  scale_x_discrete(labels = c("Male", "Female")) +
   theme(
     panel.background = element_blank(),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     axis.line = element_line(colour = "black")
   ) +
-  labs(title = "Distribution of age of patients",
-       x = "Age", y = "Number of patients")
+  labs(title = "Count of patients with CDR rating for each gender",
+       x = "Clinical Dementia Rating", y = "Count of patients"))
 
 
-# distribution of demented and non demented patients for each gender
+# distribution of total intracranial volume for each gender
 
+ggplotly(clean_data %>%
+    group_by(Gender) %>%
+    ggplot(aes(x=Gender, y=eTIV, fill=Gender)) +
+    geom_boxplot() +
+    scale_x_discrete(labels = c("Male", "Female")) +
+    coord_flip() +
+    theme(
+      panel.background = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.line = element_line(colour = "black")
+    ) +
+    labs(title = "Distribution of total Intracranial volume for each gender",
+         x = "Gender", y = "Estimated Total Intracranial Volume")
+  )
 
+# distribution of eTIV for all age groups
 
+ggplotly(clean_data %>%
+  group_by(Age) %>%
+  summarise(avg_eTIV = mean(eTIV)) %>%
+  ggplot(aes(x=Age, y=avg_eTIV, group=1)) +
+  geom_line() + geom_point(color="blue") +
+  scale_x_continuous(breaks = unique(clean_data$Age)) +
+  theme(
+    panel.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(colour = "black")
+  ) +
+  labs(title = "Average total intracranial volume for all age groups",
+       x = "Age", y = "Average Total Intracranial Volume")
+  )
 
+# correlation matrix
 
+corr_plot <- clean_data %>%
+  ggplot(aes(x=Age, y=nWBV, color=Gender)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE) +
+  scale_color_manual(values = c("deepskyblue3", "hotpink3"), 
+                     labels = c("Male", "Female")) +
+  theme(
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"),
+    legend.position = "none"
+  ) +
+  labs(title = "Correlation - Age vs whole brain volume",
+       x = "Age", y = "Whole Brain Volume")
 
-
+ggMarginal(corr_plot, type = "histogram", 
+           fill = "slategrey")
